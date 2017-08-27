f28d7df96dd42f31a4aaad0182bb809cc10c738d
Thai Bui <blquythai@hotmail.com>
2017-08-27 18:03:23 -0500

    Add all intial scrips to install Alluxio on an instance

--- deploy/ambari/recipe/install.sh
+++ deploy/ambari/recipe/install.sh
@@ -3,3 +3,12 @@ set -e
 
 # Install Centos 6 prerequisites
 source <(curl -s https://raw.githubusercontent.com/thaibui/alluxio/branch-1.5/deploy/ambari/recipe/install_centos-rhel_prerequisites.sh?$RANDOM)
+
+# Pre-install
+source <(curl -s https://raw.githubusercontent.com/thaibui/alluxio/branch-1.5/deploy/ambari/recipe/pre-install.sh?$RANDOM)
+
+# Install Alluxio
+source <(curl -s https://raw.githubusercontent.com/thaibui/alluxio/branch-1.5/deploy/ambari/recipe/install_alluxio_centos-rhel.sh?$RANDOM)
+
+# Post-install
+source <(curl -s https://raw.githubusercontent.com/thaibui/alluxio/branch-1.5/deploy/ambari/recipe/post-install.sh?$RANDOM)
