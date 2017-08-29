1e5d5b892ae150004c524e49ae294769339cd091
Thai Bui <blquythai@hotmail.com>
2017-08-29 14:23:10 -0500

    Add alluxio proxy op script

--- deploy/ambari/recipe/post-install.sh
+++ deploy/ambari/recipe/post-install.sh
@@ -4,9 +4,11 @@ set -o errexit
 
 cp /opt/alluxio/deploy/ambari/recipe/alluxio-masterd /etc/init.d/
 cp /opt/alluxio/deploy/ambari/recipe/alluxio-workerd /etc/init.d/
+cp /opt/alluxio/deploy/ambari/recipe/alluxio-proxyd /etc/init.d/
 
 chkconfig --add alluxio-masterd
 chkconfig --add alluxio-workerd
+chkconfig --add alluxio-proxyd
 
 # Add log dir
 mkdir -p /var/log/alluxio
