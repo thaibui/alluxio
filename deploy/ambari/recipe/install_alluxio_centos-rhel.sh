fe0782b7c1d8f31f18458683ae162071ce5d3f15
Thai Bui <blquythai@hotmail.com>
2017-08-31 12:09:09 -0500

    Use specific HDP version

--- deploy/ambari/recipe/install_alluxio_centos-rhel.sh
+++ deploy/ambari/recipe/install_alluxio_centos-rhel.sh
@@ -8,9 +8,13 @@ git pull
 mvn -e clean package -DskipTests -Phadoop-2.7 -Phive -Dcheckstyle.skip=true -Dlicense.skip=true -Dfindbugs.skip=true -Dmaven.javadoc.skip=true
 
 # link alluxio client runtime jar to hive-server2 and hive client auxlib directory
-mkdir -p /usr/hdp/current/hive-server2/auxlib
-mkdir -p /usr/hdp/current/hive-server2-hive2/auxlib
+
+# get the current HDP version. assuming that there's only 1 version running, else it will break the automation
+HDP_VERSION=`ls /usr/hdp/ | grep -v current`
+
+mkdir -p /usr/hdp/$HDP_VERSION/hive-server2/auxlib
+mkdir -p /usr/hdp/$HDP_VERSION/hive-server2-hive2/auxlib
 ln -s /opt/alluxio/core/client/runtime/target/alluxio-core-client-runtime-1.5.1-SNAPSHOT-jar-with-dependencies.jar \
-  /usr/hdp/current/hive-server2/auxlib/alluxio-core-client-runtime.jar
+  /usr/hdp/$HDP_VERSION/hive-server2/auxlib/alluxio-core-client-runtime.jar
 ln -s /opt/alluxio/core/client/runtime/target/alluxio-core-client-runtime-1.5.1-SNAPSHOT-jar-with-dependencies.jar \
-  /usr/hdp/current/hive-server2-hive2/auxlib/alluxio-core-client-runtime.jar
+  /usr/hdp/$HDP_VERSION/hive-server2-hive2/auxlib/alluxio-core-client-runtime.jar
