f4bf9ff76dd93eee8dfc8a29b8c6d137b68133b0
Thai Bui <blquythai@hotmail.com>
2017-09-01 14:30:11 -0500

    Add wait script to provision hive aux jar lib dirs

--- deploy/ambari/recipe/install_alluxio_centos-rhel.sh
+++ deploy/ambari/recipe/install_alluxio_centos-rhel.sh
@@ -10,11 +10,43 @@ mvn -e clean package -DskipTests -Phadoop-2.7 -Phive -Dcheckstyle.skip=true -Dli
 # link alluxio client runtime jar to hive-server2 and hive client auxlib directory
 
 # get the current HDP version. assuming that there's only 1 version running, else it will break the automation
-HDP_VERSION=`ls /usr/hdp/ | grep -v current`
+while true; do
+    if [ -d "/usr/hdp" ]; then
+        HDP_VERSION=`ls /usr/hdp/ | grep -v current`
+        while true; do
+            if [ ! -z "/usr/hdp/$HDP_VERSION" ]; then
+                while true; do
+                    if [ -d "/usr/hdp/$HDP_VERSION/hive-server2" ]; then
+                        mkdir -p /usr/hdp/$HDP_VERSION/hive-server2/auxlib
+                        ln -s /opt/alluxio/core/client/runtime/target/alluxio-core-client-runtime-1.5.1-SNAPSHOT-jar-with-dependencies.jar \
+                          /usr/hdp/$HDP_VERSION/hive-server2/auxlib/alluxio-core-client-runtime.jar
+                        break;
+                    else
+                        echo "A hive-server2 app is not installed under /usr/hdp/$HDP_VERSION. sleep 10s ..."
+                        sleep 10;
+                    fi
+                done
 
-mkdir -p /usr/hdp/$HDP_VERSION/hive-server2/auxlib
-mkdir -p /usr/hdp/$HDP_VERSION/hive-server2-hive2/auxlib
-ln -s /opt/alluxio/core/client/runtime/target/alluxio-core-client-runtime-1.5.1-SNAPSHOT-jar-with-dependencies.jar \
-  /usr/hdp/$HDP_VERSION/hive-server2/auxlib/alluxio-core-client-runtime.jar
-ln -s /opt/alluxio/core/client/runtime/target/alluxio-core-client-runtime-1.5.1-SNAPSHOT-jar-with-dependencies.jar \
-  /usr/hdp/$HDP_VERSION/hive-server2-hive2/auxlib/alluxio-core-client-runtime.jar
+                while true; do
+                    if [ -d "/usr/hdp/$HDP_VERSION/hive-server2-hive2" ]; then
+                        mkdir -p /usr/hdp/$HDP_VERSION/hive-server2-hive2/auxlib
+                        ln -s /opt/alluxio/core/client/runtime/target/alluxio-core-client-runtime-1.5.1-SNAPSHOT-jar-with-dependencies.jar \
+                          /usr/hdp/$HDP_VERSION/hive-server2-hive2/auxlib/alluxio-core-client-runtime.jar
+                        break;
+                    else
+                        echo "A hive-server2-hive2 app is not installed under /usr/hdp/$HDP_VERSION. sleep 10s ..."
+                        sleep 10;
+                    fi
+                done
+                break;
+            else
+                echo "A hdp version is not installed under /usr/hdp. sleep 10s ..."
+                sleep 10;
+            fi
+        done
+        break;
+    else
+        echo "/usr/hdp directory is not available. sleep 10s ..."
+        sleep 10;
+    fi
+done
