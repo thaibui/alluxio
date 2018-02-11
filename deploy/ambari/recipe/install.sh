82e6306cf1421b795b4a0c1d8e4853b206386073
Thai Bui <blquythai@hotmail.com>
2018-02-11 17:02:24 -0600

    Change branch-1.5 to branch-1.7

--- deploy/ambari/recipe/install.sh
+++ deploy/ambari/recipe/install.sh
@@ -2,13 +2,13 @@
 set -e
 
 # Install Centos 6 prerequisites
-source <(curl -s https://raw.githubusercontent.com/thaibui/alluxio/branch-1.5/deploy/ambari/recipe/install_centos-rhel_prerequisites.sh?$RANDOM)
+source <(curl -s https://raw.githubusercontent.com/thaibui/alluxio/branch-1.7/deploy/ambari/recipe/install_centos-rhel_prerequisites.sh?$RANDOM)
 
 # Pre-install
-source <(curl -s https://raw.githubusercontent.com/thaibui/alluxio/branch-1.5/deploy/ambari/recipe/pre-install.sh?$RANDOM)
+source <(curl -s https://raw.githubusercontent.com/thaibui/alluxio/branch-1.7/deploy/ambari/recipe/pre-install.sh?$RANDOM)
 
 # Install Alluxio
-source <(curl -s https://raw.githubusercontent.com/thaibui/alluxio/branch-1.5/deploy/ambari/recipe/install_alluxio_centos-rhel.sh?$RANDOM)
+source <(curl -s https://raw.githubusercontent.com/thaibui/alluxio/branch-1.7/deploy/ambari/recipe/install_alluxio_centos-rhel.sh?$RANDOM)
 
 # Post-install
-source <(curl -s https://raw.githubusercontent.com/thaibui/alluxio/branch-1.5/deploy/ambari/recipe/post-install.sh?$RANDOM)
+source <(curl -s https://raw.githubusercontent.com/thaibui/alluxio/branch-1.7/deploy/ambari/recipe/post-install.sh?$RANDOM)
--- deploy/ambari/recipe/install_alluxio_centos-rhel.sh
+++ deploy/ambari/recipe/install_alluxio_centos-rhel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 if [ ! -d "/opt/alluxio" ]; then
-    git clone -b branch-1.5 https://github.com/thaibui/alluxio.git /opt/alluxio
+    git clone -b branch-1.7 https://github.com/thaibui/alluxio.git /opt/alluxio
 fi
 cd /opt/alluxio
 git pull
